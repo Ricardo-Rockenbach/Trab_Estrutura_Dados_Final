b079c7688e1d8bdab28fb6c69909eefc1491a1c5
Ricardo Rockenbach <02512450040@senacrs.edu.br>
2026-06-27 11:54:29 -0300

    Main menu added

--- crud/categorias.py
+++ crud/categorias.py
@@ -3,9 +3,10 @@ from database import conexao
 cursor, conexao = conexao()
 
 def cadastrar():
-    print("Função Cadastrar categoria")
-    
-
+    sql = "INSERT INTO categoria (nome) VALUES (%s)"
+    nome = input("Digite o nome da categoria: ")
+    cursor.execute(sql, (nome,))
+    conexao.commit()
 
 def listar():
     sql = "SELECT * FROM categoria"
@@ -18,8 +19,21 @@ def listar():
 
 
 def alterar():
-    print("Função Alterar categoria")
+    print("Alterar categoria")
+    listar()
+
+    sql = "UPDATE categoria SET nome = %s WHERE id = %s"
+    id_categoria = input("Digite o ID da categoria que deseja alterar: ")
+    nome = input("Digite o novo nome da categoria: ")
+
+    cursor.execute(sql, (nome, id_categoria))
+    conexao.commit()
 
 def excluir():
-    print("Função Excluir categoria")
+    print("Excluir categoria")
+    listar()
+    sql = "DELETE FROM categoria WHERE id = %s"
+    id_categoria = input("Digite o ID da categoria que deseja excluir: ")
 
+    cursor.execute(sql, (id_categoria,))
+    conexao.commit()
--- menu.py
+++ menu.py
@@ -1,39 +1,77 @@
 from crud import categorias as cat
 from crud import livros as liv
+from rich.prompt import Prompt
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
 
 # menu principal
 def menu():
     while True:
-        print("Menu:")
-        print("1. Cadastrar categoria")
-        print("2. Listar categorias")
-        print("3. Cadastrar livro")
-        print("4. Listar livros")
-        print("5. Alterar livro")
-        print("6. Excluir livro")
-        print("7. Alterar categoria")
-        print("8. Excluir categoria")
-        print("9. Sair")
+        console.print("Menu Principal:", style="bold blue")
+        console.print("1. Categorias", style="bold green")
+        console.print("2. Livros", style="bold green")
+        console.print("3. Sair", style="bold green")
 
-        opcao = input("Escolha uma opção: ")
+        opcao = Prompt.ask("Escolha uma opção", choices=["1", "2", "3"])
+
+        if opcao == "1":
+            menu_categorias()
+        elif opcao == "2":
+            menu_livros()
+        elif opcao == "3":
+            break
+        else:
+            console.print("Opção inválida!", style="bold red")
+
+# submenu de categorias
+def menu_categorias():
+
+    while True:
+        console.print("Menu Categorias:", style="bold blue")
+        console.print("1. Cadastrar categoria", style="bold green")
+        console.print("2. Listar categorias", style="bold green")
+        console.print("3. Alterar categoria", style="bold green")
+        console.print("4. Excluir categoria", style="bold green")
+        console.print("5. Voltar", style="bold green")
+
+        opcao = Prompt.ask("Escolha uma opção", choices=["1", "2", "3", "4", "5"])
 
         if opcao == "1":
             cat.cadastrar()
         elif opcao == "2":
             cat.listar()
         elif opcao == "3":
-            liv.cadastrar()
-        elif opcao == "4":
-            liv.listar()
-        elif opcao == "5":
-            liv.alterar()
-        elif opcao == "6":
-            liv.excluir()
-        elif opcao == "7":
             cat.alterar()
-        elif opcao == "8":
+        elif opcao == "4":
             cat.excluir()
-        elif opcao == "9":
+        elif opcao == "5":
             break
         else:
-            print("Opção inválida!")
+            console.print("Opção inválida!", style="bold red")
+
+# submenu de livros
+def menu_livros():
+    while True:
+        console.print("Menu Livros:", style="bold blue")
+        console.print("1. Cadastrar livro", style="bold green")
+        console.print("2. Listar livros", style="bold green")
+        console.print("3. Alterar livro", style="bold green")
+        console.print("4. Excluir livro", style="bold green")
+        console.print("5. Voltar", style="bold green")
+
+        opcao = Prompt.ask("Escolha uma opção", choices=["1", "2", "3", "4", "5"])
+
+        if opcao == "1":
+            liv.cadastrar()
+        elif opcao == "2":
+            liv.listar()
+        elif opcao == "3":
+            liv.alterar()
+        elif opcao == "4":
+            liv.excluir()
+        elif opcao == "5":
+            break
+        else:
+            console.print("Opção inválida!", style="bold red")
